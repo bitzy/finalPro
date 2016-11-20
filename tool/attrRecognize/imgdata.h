29e683c84c1db7111afc917acaa2af51b0b1941f
sharon <zhangyan.dream@foxmail.com>
2016-11-20 15:57:23 +0800

    modified opencv namespace. eg: Mat ==> cv::Mat

--- tool/attrRecognize/imgdata.h
+++ tool/attrRecognize/imgdata.h
@@ -9,7 +9,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-using namespace cv;
+//using namespace cv;
 using namespace std;
 
 /**
@@ -36,10 +36,10 @@ private:
     //recognize process related data;    
     vector<int> attrVstd;
 
-    Mat src_ori;
-    Mat src;
-    Point2i head, neck, lshoud, rshoud, lelbow, relbow, lhand;
-    Point2i rhand, lhip, rhip, lknee, rknee, lank, rank;        
+    cv::Mat src_ori;
+    cv::Mat src;
+    cv::Point2i head, neck, lshoud, rshoud, lelbow, relbow, lhand;
+    cv::Point2i rhand, lhip, rhip, lknee, rknee, lank, rank;
     POSETYPE HSTYLE;
 
     //scale data;
@@ -65,11 +65,11 @@ public:
     void getPointColor(const int x1, const int y1, int&r, int&g, int&b) const;
 private:
     void _loadManDotstd();
-    void __resizePoint(Point& p) const;
+    void __resizePoint(cv::Point& p) const;
     void _resizePosePoints();
-    Rect __getBoundingBox();
-    void _resizeSrcToFixedHeight(const Rect& rect, int height = 800);
-    Vec3b _getPixelColor(const Point& p) const;
+    cv::Rect __getBoundingBox();
+    void _resizeSrcToFixedHeight(const cv::Rect& rect, int height = 800);
+    cv::Vec3b _getPixelColor(const cv::Point& p) const;
 };
 
 #endif // IMGDATA_H
